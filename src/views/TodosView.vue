7ae6a81f47c35da269325905e988063edacbb24a
Hussein Abou Zeinab <husseinabouzeinab@gmail.com>
2023-10-05 14:48:15 +0300

    Second Commit

--- src/views/TodosView.vue
+++ src/views/TodosView.vue
@@ -3,7 +3,10 @@ import { ref } from "vue";
 
 import { uid } from "uid";
 
+import { Icon } from '@iconify/vue';
+
 import TodoCreator from '../components/TodoCreator.vue';
+import TodoItem from '../components/TodoItem.vue'
 
 const todoList = ref([]);
 
@@ -15,12 +18,28 @@ const createTodo = (todo) => {
     isEditing: null,
   });
 };
+
+const toggleTodoComplete = (todoPos) => {
+  todoList.value[todoPos].isCompleted = !todoList.value[todoPos].isCompleted;
+};
 </script>
 
 <template>
   <main>
     <h1>Create Todo</h1>
     <TodoCreator @create-todo="createTodo" />
+    <ul class="todo-list" v-if="todoList.length > 0">
+      <TodoItem
+      v-for="{todo, index} in todoList"
+      :todo="todo"
+      :index="index"
+      @toggle-complete="toggleTodoComplete"
+      />
+    </ul>
+    <p class="todos-msg" v-else>
+      <Icon icon="noto-v1:sad-but-relieved-face" width="22" />
+      <span>You have no todo's to complete! Add one!</span>
+    </p>
   </main>
 </template>
 
@@ -36,6 +55,22 @@ main {
   h1 {
     margin-bottom: 16px;
     text-align: center;
+  }
+
+  .todo-list {
+    display: flex;
+    flex-direction: column;
+    list-style: none;
+    margin-top: 24px;
+    gap: 20px;
+  }
+
+  .todos-msg {
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    gap: 8px;
+    margin-top: 24px;
   }
 }
 </style>
